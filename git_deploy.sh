9e0b9cc0d67efce8d89bf3cfdb685a62c8dbd821
isbaysoft <isbaysoft@gmail.com>
2012-08-14 18:09:49 +0300

    unicorn binding

--- git_deploy.sh
+++ git_deploy.sh
@@ -9,7 +9,7 @@ kill -9 `cat tmp/pids/unicorn.pid`
 sleep 5
 kill -9 `cat tmp/pids/unicorn.pid`
 sleep 2
-RAILS_ENV=production bundle exec unicorn_rails -o 127.0.0.1 -c config/unicorn.rb -E production -D -p 3001
+RAILS_ENV=production bundle exec unicorn_rails -c config/unicorn.rb -E production -D -p 3001
 
 echo "RRauth deployment done"
 
